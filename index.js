3288fb5a2013180cf680d53b2da3f453dca98e95
DumBo456 <157987081+DumBo456@users.noreply.github.com>
2024-01-30 18:16:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,149 +1,56 @@
-const { Client, RichPresence } = require('discord.js-selfbot-v13');
-const express = require('express');
-const app = express();
-const bot = new Client({ checkUpdate: false });
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
+});
 
+const keepAlive = require('./server.js');
+keepAlive();
 
-const textmain = "";
-const textthree = "";
-const textfour = "";
-const type = "STREAMING";
-const pictureGede = [
-"https://cdn.discordapp.com/attachments/1142012457783656478/1142410200570155018/88cd12b61a7efc2ba0679356f450559a.gif?ex=65c754e8&is=65b4dfe8&hm=59f71b434beda4b08a235e944e8acc91b41d51bace13927ba0c8a39c84d07f43&",
-                         ];
-const smallpictureGede = [
-  "https://cdn.discordapp.com/attachments/1148099836537753672/1201545938611748955/C2A3D941-3584-469B-84C0-4395A291AAB8.gif?ex=65ca35db&is=65b7c0db&hm=803e593980963eb618e2a9689ae713f53ff2156256ea2355eefe39daf160d4fe&",
-                    ];
-const nameButtonone = "";
-let linkButtonone = "";
-
-const xxxx = ['★☆★☆★☆★','☆★☆★☆★☆'];
-
-const randomTexts = [
-  'FaceBook',
-
-
-];
-
-const randomtext3 = [
-  '平和は微笑みから始まります。',
-  '大きな野望は偉大な人を育てる.',
-  '夢見ることができれば、それは実現できる。',
-  '私の人生は楽しくなかった。だから私は自分の人生を創造したの。',
-   'すべては練習のなかにある。'
-
-];
-
-const randomtext4 = [
-  '❡',
-  '☬',
-  '✞',
-  '✡'
-
-
-];
-
-const randomtext5 = [];
-
-const randomLinks = [
-  "https://www.facebook.com/dumbohz?mibextid=LQQJ4d",
-];
-
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-function getRandomElement(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
-function getTimestampsForDay(year, month, day) {
-  const date = new Date(Date.UTC(year, month, day));
-  date.setHours(date.getHours() + 7);
-  date.setUTCHours(0, 0, 0, 0);
-  const timestampMidnight = date.getTime();
-  date.setUTCHours(23, 59, 59, 999);
-  const timestampEndOfDay = date.getTime();
-
-  return {
-    timestampMidnight,
-    timestampEndOfDay
+function formatTime() { //Credits to himika#0001 and never#0001
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
   };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
 }
 
+client.on('ready', async () => {
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`);
 
+  const r = new Discord.RichPresence()
+    .setApplicationId('1055150665560752178')
+    .setType('STREAMING')
+    .setURL('https://www.youtube.com/watch?v=HOGfrOK9a84') //Must be a youtube video link 
+    .setState('DumBoHz')
+    .  setName('Test')
+    .setDetails(`••••[${formatTime()}]`)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1142012457783656478/1142040285526892594/8f1e237076159c35cb5124b744b451cf.gif?ex=65c5fc66&is=65b38766&hm=e4a2c4a6ea51384ea76853752c2e90f62751bf8eded42cb71c324e05c0ddead4&') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('$$$') //Text when you hover the Large image
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1142012457783656478/1142040284721590313/0e328ebe0cf12640b9b2e9db7b3e7dac.gif?ex=65c5fc65&is=65b38765&hm=d8c6996ad1487be141fc1a701d1d4289d9c53bac7c91b17767dcca8e1b2c675e&' ) //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Test') //Text when you hover the Small image
+    .addButton('Watch', 'https://youtube.com/@DumBo22?si=qUrUhbdjzXRpe5aj')
+    .addButton('Donate', 'https://www.facebook.com/dumbohz?mibextid=LQQJ4d');
 
-app.all('/', (req, res) => {
-  res.send("NOW GO TO uptimerobot.com AND PASTE THIS URL");
-});
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "Test" }); //dnd, online, idle, offline
 
-app.listen(8080, () => {
-  console.log("Server is ready!!");
-});
-
-bot.on('debug', (a) => {
-  if (a.startsWith("Hit a 429")) process.kill(1);
-});
-
-bot.on('ready', async () => {
+  let prevTime = null;
   setInterval(() => {
-    const date = new Date();
-    const datee = date.getDate();
-    const hours = (date.getHours() + 7) % 24;
-    const minutes = date.getMinutes();
-    const months = date.getMonth();
-    const year = date.getFullYear();
-    const timestamps = getTimestampsForDay(year, months, datee);
-    function addZero(number) {
-  return number < 10 ? "0" + number : number;
-}
-
-  function getRandomNumber() {
-    return Math.random() * (30 - 20) + 20;
-  }
-  function getWRandomNumber() {
-    return Math.random() * (5 - 3) + 3;
-  }
-  function roundToDecimal(number) {
-    return Math.round(number * 10) / 10;
-  }
-
-  const randomNumber = getRandomNumber();
-  const roundedNumber = roundToDecimal(randomNumber);
-  const roundwNumber = roundToDecimal(getWRandomNumber());
-    const month = addZero(months+1);
-    const dates = addZero(datee)
-    const resultthree = "Badminton"
-
-
-    // Randomize the link and text for the button
-    randomXX = getRandomElement(xxxx);
-    rdpictureGede = getRandomElement(pictureGede);
-    rdsmallpictureGede = getRandomElement(smallpictureGede);
-    const randomButtonText = getRandomElement(randomTexts);
-    const randomButtonText3 = getRandomElement(randomtext3);
-    const randomButtonText4 = getRandomElement(randomtext4);
-    const randomButtonText5 = getRandomElement(randomtext5);
-    linkButtonone = getRandomElement(randomLinks);
-    const pr = new RichPresence()  
-      .setName(`Honkai Star Rail`)
-      .setURL('https://www.youtube.com/watch?v=tE_-be3OnCk')
-      .setType(`${type}`.toUpperCase())
-      .setApplicationId("1055150665560752178")
-      .setAssetsLargeImage(`${rdpictureGede}`)
-      .setAssetsSmallImage(`${rdsmallpictureGede}`)
-      .setAssetsLargeText(`
-🌡${roundedNumber} °C ⋆ 🍃 ${roundwNumber} m/s`)
-      .setAssetsSmallText(`ping: ${bot.ws.ping}ms`)
-      .setState(`: ${randomXX}`)
-      .setDetails(`${randomButtonText3}`)
-      .setStartTimestamp(timestamps.timestampMidnight)
-      .setEndTimestamp(timestamps.timestampEndOfDay)
-      .addButton(`${randomButtonText}`, `${linkButtonone}`)
-      .addButton(`${randomButtonText4}`, `${linkButtonone}`)
-    bot.user.setActivity(pr.toJSON());
-  }, getRandomInt(1000, 3000));
-  console.log(`${bot.user.tag} Status is showed up !!`);
+    const newTime = formatTime();
+    if (newTime !== prevTime) {
+      const newDetails = ` [${newTime}]`;
+      r.setDetails(newDetails);
+      client.user.setActivity(r);
+      prevTime = newTime;
+    }
+  }, 1000); // Update every second
 });
 
-bot.login(process.env['TOKEN']);
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
